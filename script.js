38378c1f172a75487763a81bc326dcd391f34f2e
NiedsonBarros <118897579+NiedsonBarros@users.noreply.github.com>
2022-11-30 11:35:02 -0300

    Ajustes no HTML

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function calc() {
+function calc() {resultado
     let modo = document.getElementById("modoVini").value.toUpperCase();
 
     switch (modo) {
-        case "FELIZ": return document.getElementById("informacoes").innerHTML =
+        case "FELIZ": return document.getElementById("resultado").innerHTML =
             `<p>FELIZ!!</p>
 <img src="./feliz.png" style="height: 500px">`
             break;
-        case "TRABALHADOR": return document.getElementById("informacoes").innerHTML =
+        case "TRABALHADOR": return document.getElementById("resultado").innerHTML =
             `<p>TRABALHADOR!!</p>
-<img src="./trabalhador.png" style="height: 500px">`
+<img src="./trabalhador.png" style="height: 400px; width: 700px";>`
             break;
-        case "FESTEIRO": return document.getElementById("informacoes").innerHTML =
+        case "FESTEIRO": return document.getElementById("resultado").innerHTML =
             `<p>FESTEIRO!!</p>
-<img src="./festeiro.png" style="height: 500px">`
+<img src="./festeiro.png" style="height: 400px; width: 700px";>`
             break;
-        case "SENSUAL": return document.getElementById("informacoes").innerHTML =
+        case "SENSUAL": return document.getElementById("resultado").innerHTML =
             `<p>SENSUAL!!</p>
 <img src="./sensual.png" style="height: 500px">`
             break;
-        case "SENSUAL DEMAIS": return document.getElementById("informacoes").innerHTML =
+        case "SENSUAL DEMAIS": return document.getElementById("resultado").innerHTML =
             `<p>SENSUAL!!</p>
 <img src="./sensual2.png" style="height: 500px">`
             break;
-        case "SENSUAL DEMAIS DA CONTA": return document.getElementById("informacoes").innerHTML =
+        case "SENSUAL DEMAIS DA CONTA": return document.getElementById("resultado").innerHTML =
             `<p>SENSUAL DEMAIS DA CONTA!!</p>
 <img src="./sensual3.png" style="height: 500px">`
             break;
